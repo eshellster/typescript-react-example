c4a278c869aac220c2baa135f68195d43002f2bc
eshell <eshellster@gmail.com>
2020-07-18 10:38:10 +0900

    Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { TextField } from "./TextField";
-import { ReducerExample } from "./ReducerExample";
+import { Counter } from "./Counter";
 
 const App: React.FC = () => {
   return (
     <div>
-      <TextField
-        text="hello"
-        person={{ firstName: "e", lastName: "shell" }}
-        handleChange={(event) => {}}
-      />
-      <ReducerExample />
+      <Counter>
+        {(count, setCount) => (
+          <div>
+            <button onClick={() => setCount(count - 1)}>-</button>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
     </div>
   );
 };
